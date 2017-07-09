56c022d058d28700805020fec7cd2d588c0a8843
Francisco José Juan Quintanilla <frnjjq@gmail.com>
2017-07-09 17:35:55 +0200

    Continue the implementation of the camera module.
    -Added some documentation to the module.
    -Refined some of the functions.

--- camera_reader.c
+++ camera_reader.c
@@ -12,7 +12,19 @@
 
 #include "camera_reader.h"
 
+#include "bcm_host.h"
+#include "interface/vcos/vcos.h"
+
+#include "interface/mmal/mmal.h"
+#include "interface/mmal/mmal_logging.h"
+#include "interface/mmal/mmal_buffer.h"
+#include "interface/mmal/util/mmal_util.h"
+#include "interface/mmal/util/mmal_util_params.h"
+#include "interface/mmal/util/mmal_default_components.h"
+
 #define MMAL_CAMERA_VIDEO_PORT 0
+#define VIDEO_FRAME_RATE_DEN 1
+#define VIDEO_OUTPUT_BUFFERS_NUM 1
 
 static MMAL_COMPONENT_T init_camera(CAMERA_OPTIONS *options, ENCODE_CALLBACK cb)
 {
@@ -20,17 +32,17 @@ static MMAL_COMPONENT_T init_camera(CAMERA_OPTIONS *options, ENCODE_CALLBACK cb)
   MMAL_ES_FORMAT_T *format;
   MMAL_PORT_T *video_port = NULL;
   MMAL_STATUS_T status;
-  MMAL_POOL_T *pool;
+  MMAL_POOL_T *pool= 0;
 
   // Create the camera component
   status = mmal_component_create(MMAL_COMPONENT_DEFAULT_CAMERA, &camera);
 
   if (status != MMAL_SUCCESS)
   {
-     fprintf( stderr, "Failed to create camera component");
+     fprintf(stderr, "Error creating camera component\nError -> %d\nStatus -> %s", status, mmal_status_to_string(status));
      goto error;
   }
-
+  // Select the Camera from options
   MMAL_PARAMETER_INT32_T camera_num =
      {{MMAL_PARAMETER_CAMERA_NUM, sizeof(camera_num)}, options->cameraNum};
 
@@ -38,59 +50,64 @@ static MMAL_COMPONENT_T init_camera(CAMERA_OPTIONS *options, ENCODE_CALLBACK cb)
 
   if (status != MMAL_SUCCESS)
   {
-     vcos_log_error("Could not select camera : error %d", status);
+     fprintf(stderr,"Could not select camera\nError -> %d\nStatus -> %s", status, mmal_status_to_string(status));
      goto error;
   }
-
+  // Check that the camera has enough outputs
   if (!camera->output_num)
   {
      status = MMAL_ENOSYS;
-     vcos_log_error("Camera doesn't have output ports");
+     fprintf(stderr,"Camera doesn't have output ports\nError -> %d\nStatus -> %s", status, mmal_status_to_string(status));
      goto error;
   }
-
+  // Set Mode of the camera
   status = mmal_port_parameter_set_uint32(camera->control, MMAL_PARAMETER_CAMERA_CUSTOM_SENSOR_CONFIG,options->sensor_mode); // Mode is set to 1 @see https://www.raspberrypi.org/documentation/raspbian/applications/camera.md  for camera possible modes.
 
   if (status != MMAL_SUCCESS)
   {
-     vcos_log_error("Could not set sensor mode : error %d", status);
+     fprintf(stderr,"Could not set sensor mode\nError -> %d\nStatus -> %s", status, mmal_status_to_string(status));
      goto error;
   }
 
-  // Enable the camera, and tell it its control callback function
+  MMAL_PARAMETER_CAMERA_CONFIG_T cam_config =
+  {
+     { MMAL_PARAMETER_CAMERA_CONFIG, sizeof(cam_config) },
+     .max_stills_w = options->width,
+     .max_stills_h = options->height,
+     .stills_yuv422 = 0,
+     .one_shot_stills = 0,
+     .max_preview_video_w = options->width,
+     .max_preview_video_h = options->height,
+     .num_preview_video_frames = 3,
+     .stills_capture_circular_buffer_height = 0,
+     .fast_preview_resume = 0,
+     .use_stc_timestamp = MMAL_PARAM_TIMESTAMP_MODE_RESET_STC
+  };
+  status = mmal_port_parameter_set(camera->control, &cam_config.hdr);
+
+  if (status != MMAL_SUCCESS)
+  {
+     fprintf(stderr,"Could not set camera config\nError -> %d\nStatus -> %s", status, mmal_status_to_string(status));
+     goto error;
+  }
+
+  // TODO-> Much more parameters can be set on the control port in order to alterate the camera behaviour. Explore those in the future.
+
+  // As we finished with the control port we have to enable it giving it a callback function for events generated and stuff.
   status = mmal_port_enable(camera->control, camera_control_callback);
 
   if (status != MMAL_SUCCESS)
   {
-     vcos_log_error("Unable to enable control port : error %d", status);
+     fprintf(stderr,"Unable to enable control port\nError -> %d\nStatus -> %s", status, mmal_status_to_string(status));
      goto error;
   }
 
+  // Obtain the output port from the camera to configure it.
    video_port = camera->output[MMAL_CAMERA_VIDEO_PORT];
 
-   //  set up the camera configuration
-   {
-      MMAL_PARAMETER_CAMERA_CONFIG_T cam_config =
-      {
-         { MMAL_PARAMETER_CAMERA_CONFIG, sizeof(cam_config) },
-         .max_stills_w = options->width,
-         .max_stills_h = options->height,
-         .stills_yuv422 = 0,
-         .one_shot_stills = 0,
-         .max_preview_video_w = options->width,
-         .max_preview_video_h = options->height,
-         .num_preview_video_frames = 3,
-         .stills_capture_circular_buffer_height = 0,
-         .fast_preview_resume = 0,
-         .use_stc_timestamp = MMAL_PARAM_TIMESTAMP_MODE_RESET_STC
-      };
-      mmal_port_parameter_set(camera->control, &cam_config.hdr);
-   }
-
-  format = video_port->format;
+   format = video_port->format;
 
    format->encoding = MMAL_ENCODING_I420; // @See https://www.fourcc.org/pixel-format/yuv-i420/
-   format->encoding_variant = MMAL_ENCODING_I420; // @See https://www.fourcc.org/pixel-format/yuv-i420/
 
    format->es->video.width = VCOS_ALIGN_UP(options->width, 32);
    format->es->video.height = VCOS_ALIGN_UP(options->height, 16);
@@ -105,7 +122,7 @@ static MMAL_COMPONENT_T init_camera(CAMERA_OPTIONS *options, ENCODE_CALLBACK cb)
 
    if (status != MMAL_SUCCESS)
    {
-      vcos_log_error("camera video format couldn't be set");
+      fprintf(stderr,"camera video format couldn't be set\nError -> %d\nStatus -> %s", status, mmal_status_to_string(status));
       goto error;
    }
 
@@ -113,7 +130,7 @@ static MMAL_COMPONENT_T init_camera(CAMERA_OPTIONS *options, ENCODE_CALLBACK cb)
 
    if (status != MMAL_SUCCESS)
    {
-      vcos_log_error("camera component couldn't be enabled");
+      vcos_log_error("camera component couldn't be enabled\nError -> %d\nStatus -> %s", status, mmal_status_to_string(status));
       goto error;
    }
 
@@ -124,7 +141,7 @@ static MMAL_COMPONENT_T init_camera(CAMERA_OPTIONS *options, ENCODE_CALLBACK cb)
    status = mmal_port_parameter_set_boolean(video_port, MMAL_PARAMETER_ZERO_COPY, MMAL_TRUE);
    if (status != MMAL_SUCCESS)
    {
-      vcos_log_error("Failed to select zero copy");
+      vcos_log_error("Failed to select zero copy\nError -> %d\nStatus -> %s", status, mmal_status_to_string(status));
       goto error;
    }
 
@@ -135,8 +152,26 @@ static MMAL_COMPONENT_T init_camera(CAMERA_OPTIONS *options, ENCODE_CALLBACK cb)
    {
       vcos_log_error("Failed to create buffer header pool for camera still port %s", video_port->name);
    }
+   PORT_USERDATA callback_data;
+   callback_data->pool = pool;
+   callback_data->cb= &cb
+   camera_video_port->userdata = (struct MMAL_PORT_USERDATA_T *)&callback_data;
 
-   camera_video_port->userdata = (struct MMAL_PORT_USERDATA_T *)&cb;
+   // Send all the buffers to the camera video port
+   {
+      int num = mmal_queue_length(pool->queue);
+      int q;
+      for (q=0;q<num;q++)
+      {
+         MMAL_BUFFER_HEADER_T *buffer = mmal_queue_get(pool->queue);
+
+         if (!buffer)
+            vcos_log_error("Unable to get a required buffer %d from pool queue", q);
+
+         if (mmal_port_send_buffer(camera_video_port, buffer)!= MMAL_SUCCESS)
+            vcos_log_error("Unable to send a buffer to camera video port (%d)", q);
+      }
+   }
 
    status = mmal_port_enable(camera_video_port, camera_buffer_callback);
 
@@ -146,15 +181,15 @@ static MMAL_COMPONENT_T init_camera(CAMERA_OPTIONS *options, ENCODE_CALLBACK cb)
       goto error;
    }
 
-
   return camera;
 
 error:
 
   if (camera)
-     mmal_component_destroy(camera);
-
-  return status;
+    mmal_component_destroy(camera);
+  if (pool)
+    mmal_pool_destroy(pool);
+  return 0;
 }
 
 
@@ -167,9 +202,26 @@ static void camera_control_callback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buf
 
 static void camera_buffer_callback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer)
 {
+  // TODO -> I need the pool in the userdata
+  port->userdata->cb(buffer->data,buffer->offset, buffer->length,buffer->pts);
 
-  port->userdata(buffer->data,buffer->offset, buffer->length,buffer->pts);
+  // release buffer back to the pool
   mmal_buffer_header_release(buffer);
+
+  // and send one back to the port (if still open)
+  if (port->is_enabled)
+  {
+     MMAL_STATUS_T status;
+     MMAL_BUFFER_HEADER_T *new_buffer;
+
+     new_buffer = mmal_queue_get(port->userdata->pool->queue);
+
+     if (new_buffer)
+        status = mmal_port_send_buffer(port, new_buffer);
+
+     if (!new_buffer || status != MMAL_SUCCESS)
+        vcos_log_error("Unable to return a buffer to the camera port");
+  }
 }
 
 
--- camera_reader.h
+++ camera_reader.h
@@ -10,29 +10,60 @@
  * @see https://github.com/jjaranda13/LHE_Pi
  */
 
+ /**
+  * Options to pass to the camera. All are mandatory.
+  */
 struct CAMERA_OPTIONS
 {
-  int timeout;                        /// Time taken before frame is grabbed and app then shuts down. Units are milliseconds
   int width;                          /// Requested width of image
   int height;                         /// requested height of image
   int framerate;                      /// Requested frame rate (fps)
-  int verbose;                        /// !0 if want detailed run information
   int cameraNum;                       /// Camera number. Set it to 0 as default.
   int sensor_mode;                     /// Sensor mode. 0=auto. Check docs/forum for modes selected by other values.
 }
 
-struct CAMERA_BUFFER
-{
-  MMAL_COMPONENT_T camera_component;
-  MMAL_BUFFER_HEADER_T buffer_header;
-}
-
+/**
+ * @brief Callback function that is called whne a frame is ready.
+ *
+ * The function to be called recieves a buffer with its indexes to obtain the
+ * frame. A timestamp of the presentation time is obtained too.
+ *
+ * @p data Array of bytes contaning the image.
+ * @p offset Offset of the start of the frame.
+ * @p length Lengh of the payload. Counter after offset.
+ * @p pts Presentation timestamp of the frame.
+ */
 typedef void(* ENCODE_CALLBACK) (uint8_t* data, uint32_t *offset, uint32_t length, int64_t pts);
 
+/**
+ * @brief Initiates the camera.
+ *
+ * This functions initites the camera and starts trwing frame to the callabck provided.
+ *
+ * @param options Options for ythe video capture.
+ * @param cb callabck function to eb executed after a frame
+ * @return MMAL_COMPONENT_T the camera component.
+ */
 static MMAL_COMPONENT_T init_camera(CAMERA_OPTIONS *options, ENCODE_CALLBACK cb);
 
+/**
+ * @brief Closes the camera passed.
+ *
+ *Stops and closes the camera passed.
+ *
+ * @param camera_object the camera to be closed.
+ * @return Status of the process.
+ */
+int close_camera(camera_object);
+
+
+// Private area
 static void camera_control_callback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer);
 
 static void camera_buffer_callback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer);
 
-int close_camera(camera_object);
+typedef struct
+{
+   MMAL_POOL_T *pool;                   /// Pool in which the port has its buffer_headers
+   ENCODE_CALLBACK cb;                 /// pointer to the callback
+} PORT_USERDATA;
