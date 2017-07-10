6de4f3fdeb805ea0f03ae0ba9dd2717be3856be3
jjaranda13 <jjaranda13@hotmail.com>
2017-07-10 13:40:34 +0000

    quantizer2

--- quantizer.h
+++ quantizer.h
@@ -3,4 +3,5 @@
 void init_quantizer();
 
 void quantize_scanline(char **orig_YUV, int line,int width, unsigned char **hops,unsigned char **result_YUV);
-void compute_delta(char **orig_YUV, int y,int width, unsigned char **delta) ;
+
+void compute_delta(unsigned char **frame_new,unsigned char **frame_prev, int y,int width, unsigned char **delta);
