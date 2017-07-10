20bc96492cf479661a059d3e17ef990eb0a7507a
jjaranda13 <jjaranda13@hotmail.com>
2017-07-10 09:20:33 +0000

    quantizer

--- quantizer.c
+++ quantizer.c
@@ -55,7 +55,7 @@ for (int hop0=0;hop0<=255;hop0++)
 
 }// end function
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-void quantize_scanline(char **orig_YUV, int line,int width, char **hops,char **result_YUV) {
+void quantize_scanline(char **orig_YUV, int y,int width, char **hops,char **result_YUV) {
 /// this function quantize the luminances or chrominances of one scanline
 /// inputs : orig_YUV, line, width,
 /// outputs: hops, result_YUV
@@ -66,39 +66,47 @@ char min_h1=4;
 char start_h1=(max_h1+min_h1)<<1;
 char h1=start_h1;
 char last_hop=4;
-char oc=0;//original color
+
 bool last_small_hop=true; //last hop was small
 bool small_hop=true;//current hop is small
-int hop0=0; //prediction
+char hop0=0; //prediction
 char emin=255;//error min
 char error=0;//computed error
-char quantum=0; //quantum value
-// to do
-//first hop is original signal value
-hops[line][0]=image[line][0];
-int pix=line*width+1;
+char oc=orig_YUV[y][0];//original color
+char quantum=oc; //quantum value
+char hop_number=4;
+unsigned char hop_value=0;//data from cache
+
+
+//first hop is original signal value, and also result_YUV
+hops[y][0]=quantum;
+result_YUV[y][0]=quantum;
+
+
+
+
 
 for (int x=1;x<width;x++)
   {
 
   // --------------------- PHASE 1: PREDICTION---------------------------------------------------------
   //original color
-  oc=image[line][x];
+  oc=orig_YUV[y][x];
 
-  if (y>0 && x!=img.width-1)
+  if (y>0 && x!=width-1)
     {
 
-    if (last_small_hop) hop0=(result_YUV[pix-1]+result_YUV[pix-width]+result_YUV[pix-width+1])/3;
-    else hop0=(result_YUV[pix-1]+result_YUV[pix-width+1])>>1;
+    if (last_small_hop) hop0=(result_YUV[y][x-1]+result_YUV[y-1[x]]+result_YUV[y-1][x+1])/3;
+    else hop0=(result_YUV[y][x-1]+result_YUV[y-1][x+1])>>1;
 
     }
   else if (y>0)//x=width
     {
-    hop0=(result_YUV[pix-1]+result_YUV[pix-width])>>1;
+    hop0=(result_YUV[y][x-1]+result_YUV[y-1][x])>>1;
     }
   else //y=0
    {
-    hop0=result_YUV[pix-1];
+    hop0=result_YUV[y][x-1];
    }
 
 
@@ -129,7 +137,7 @@ for (int x=1;x<width;x++)
       // case hops 6 to 8 (less frequent)
       for (int i=3;i<6;i++)
         {
-        hop_value=cache_hopps[hop0][hop1-4][i];//indexes are 3 to 5
+        hop_value=cache_hops[hop0][h1-4][i];//indexes are 3 to 5
         error=oc-hop_value;
         if (error<0) error=-error;
         if (error<emin)
@@ -166,7 +174,7 @@ for (int x=1;x<width;x++)
       // case hops 2 to 0 (less frequent)
       for (int i=2;i>=0;i--)
         {
-        hop_value=cache_hopps[hop0][hop1-4][i];//indexes are 2 to 0
+        hop_value=cache_hops[hop0][h1-4][i];//indexes are 2 to 0
         error=oc-hop_value;
         if (error<0) error=-error;
         if (error<emin)
@@ -199,5 +207,6 @@ for (int x=1;x<width;x++)
     {
     h1=max_h1;
     }
+  last_small_hop=small_hop;
   }
 }
--- quantizer.h
+++ quantizer.h
@@ -1,6 +1,7 @@
 
 
 void init_quantizer();
-void quantize_scanline(char **image, int line,int width, char **hops);
+
+void quantize_scanline(char **orig_YUV, int line,int width, char **hops,char **result_YUV);
 
 
