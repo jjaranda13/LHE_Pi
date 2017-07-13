9cd05ce475f39b15361915a976118a59e498c787
m-alarcon <m.alarcongra@gmail.com>
2017-07-13 15:17:08 +0200

    Clean downsampler

--- downsampler.c
+++ downsampler.c
@@ -6,35 +6,25 @@
 #include "downsampler.h"
 
 /*Función de downsampling de un scanline*/
-void down_avg_horiz(char **imagen_capturada, int linea) {
-pppx = 2;
-switch(pppx){
+void down_avg_horiz(unsigned char *imagen_capturada, int line) {
+	pppx = 2;
+	switch(pppx){
+		case 2:
 
-case 2:
+			for (int i=0; i < sizeof(imagen_capturada[line]); i+=2) {
+				scanlines[line][i] =  (imagen_capturada[line][i<<1]+imagen_capturada[line][((i+1)<<1)]) >>1; //px[1]+px[2]/2
+			}
 
+		default:
 
-
-	for (int i=0; i < sizeof(imagen_capturada[linea]); i++) {//Esto habra que modificarlo dependiendo de como se almacenen los datos que nos da la camara
-		//for (int j=0; j < pppx; j++) {
-		//	suma += imagen_capturada[linea][(i*pppx)+j];
-		//}
-		//scanlines[linea][i] = suma/pppx;
-		scanlines[linea][i] =  (imagen_capturada[linea][i<<1]+imagen_capturada[linea][((i+1)<<1)]) >>1;
-		//>> 1;
-		//char a=imagen_capturada[linea][i*pppx];
-		//scanlines[linea][i] =  (a+a)>>1;
+		    for (int i=0; i < sizeof(imagen_capturada[line]); i+=pppx) {
+		    	scanlines[line][i] = 0;
+		    	for (int j=0; j < pppx; j++) {
+					scanlines[line][i] += imagen_capturada[line][(i+j)*pppx];
+		    	}
+	    		scanlines[line][i]/pppx;
+			}
 	}
-
-default:
-
-   for (int i=0; i < sizeof(imagen_capturada[linea])/2; i+=2) {//Esto habra que modificarlo dependiendo de como se almacenen los datos que nos da la camara
-
-		scanlines[linea][i] =  (imagen_capturada[linea][i<<1]+imagen_capturada[linea][((i+1)<<1)]) >>1;
-
-	}
-
-
-}
 }
 
 
--- entropic_enc.c
+++ entropic_enc.c
@@ -1,1 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <stdbool.h>
+#include <math.h>
+//#include <limits.h>
+#include "main.h"
+#include "entropic_enc.h"
 
+#define testBit(A,k) ((A & (1<<(k)))>>k)
+unsigned char *huff_table;
+unsigned char rlc_length, condition_length;
+
+double timeval_diff(struct timeval *a, struct timeval *b) {
+	return ((double)(a->tv_sec +(double)a->tv_usec/1000000)-(double)(b->tv_sec + (double)b->tv_usec/1000000));
+}
+
+void init_huffmann_table() {
+
+	huff_table = malloc(9);
+	huff_table[0] = 0b00000000;
+	huff_table[1] = 0b00000010;
+	huff_table[2] = 0b00001000;
+	huff_table[3] = 0b00100000;
+	huff_table[4] = 0b10000000;
+	huff_table[5] = 0b01000000;
+	huff_table[6] = 0b00010000;
+	huff_table[7] = 0b00000100;
+	huff_table[8] = 0b00000001;
+
+}
+
+/*
+		switch (hops[line][i]) {
+			case 4:
+
+		}
+*/
+
+
+
+
+void entropic_enc(char **hops, int line) {
+
+	unsigned int counterh0 = 0;
+	unsigned int counterh0_prev = 0;
+	unsigned char mode = 0;
+	unsigned char mode_prev = mode;
+	unsigned char rlc_length_ini = 4;
+	unsigned char condition_length_ini = 7;
+
+	rlc_length = rlc_length_ini;
+	condition_length = condition_length_ini;
+
+	for (int i = 0; i < sizeof(hops[line]); i++) {
+		counterh0_prev = counterh0;
+		if (hops[line][i]==4){
+			counterh0+=1;
+			if (mode==0 && counterh0==condition_length) {mode=1; counterh0=0;}
+		} else {
+			mode=0;
+			counterh0=0;
+		}
+
+		if (mode==1 && counterh0==(unsigned char)(pow(2, rlc_length)-1)) {
+			counterh0=0;
+			rlc_length=5;
+		}
+
+		if (mode_prev==1 && mode==0) {
+			condition_length=condition_length_ini;
+			rlc_length=rlc_length_ini;
+		}
+
+
+
+
+
+	}
+
+}
+
+int main(int argc, char* argv[]) {
+
+	struct timeval t_ini, t_fin;
+	double secs;
+
+	gettimeofday(&t_ini, NULL);
+
+	init_huffmann_table();
+	for (int j = 0; j < 9; j++){
+		for (int i = 7; i >= 0; i--){
+			printf("%u", testBit(huff_table[j], i));
+			if (testBit(huff_table[j], i) == 1) {
+				break;
+			}
+		}
+		printf("\n");
+	}
+
+	gettimeofday(&t_fin, NULL);
+
+	secs = timeval_diff(&t_fin, &t_ini)/10000;
+	printf("%.16g ms\n", secs * 1000.0);
+
+	return 0;
+}
